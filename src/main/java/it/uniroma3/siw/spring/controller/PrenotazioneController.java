a35388eff3562d2660ce2455f60f46d4a534ab73
pelatux3 <72656722+pelatux3@users.noreply.github.com>
2021-07-02 15:36:07 +0200

    create le classi java per la prenotazione

---
+++ src/main/java/it/uniroma3/siw/spring/controller/PrenotazioneController.java
@@ -0,0 +1,54 @@
+package it.uniroma3.siw.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import it.uniroma3.siw.spring.model.Prenotazione;
+import it.uniroma3.siw.spring.service.PrenotazioneService;
+
+@Controller
+public class PrenotazioneController {
+	
+	@Autowired
+	private PrenotazioneService prenotazioneService;
+	
+    @Autowired
+    private PrenotazioneValidator prenotazioneValidator;
+    
+
+    @RequestMapping(value="/addPrenotazione", method = RequestMethod.GET)
+    public String addPrenotazione(Model model) {
+    	model.addAttribute("prenotazione", new Prenotazione());
+        return "prenotazioneForm.html";
+    }
+
+    @RequestMapping(value = "/prenotazione/{id}", method = RequestMethod.GET)
+    public String getPrenotazione(@PathVariable("id") Long id, Model model) {
+    	model.addAttribute("prenotazione", this.prenotazioneService.prenotazionePerId(id));
+    	return "prenotazione.html";
+    }
+
+    @RequestMapping(value = "/prenotazione", method = RequestMethod.GET)
+    public String getPrenotazioni(Model model) {
+    		model.addAttribute("prenotazioni", this.prenotazioneService.tutti());
+    		return "prenotazioni.html";
+    }
+    
+    @RequestMapping(value = "/prenotazione", method = RequestMethod.POST)
+    public String newPrenotazione(@ModelAttribute("prenotazione") Prenotazione prenotazione, 
+    									Model model, BindingResult bindingResult) {
+    	this.prenotazioneValidator.validate(prenotazione, bindingResult);
+        if (!bindingResult.hasErrors()) {
+        	this.prenotazioneService.inserisci(prenotazione);
+            model.addAttribute("prenotazioni", this.prenotazioneService.tutti());
+            return "prenotazioni.html";
+        }
+        return "prenotazioneForm.html";
+    }
+}
---
+++ src/main/java/it/uniroma3/siw/spring/repository/PrenotazioneRepository.java
@@ -0,0 +1,15 @@
+package it.uniroma3.siw.spring.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.spring.model.Prenotazione;
+
+public interface PrenotazioneRepository extends CrudRepository<Prenotazione, Long> {
+
+	public List<Prenotazione> findById(String idPrenotazione);
+	
+	public List<Prenotazione> findByData(LocalDate data);
+}
